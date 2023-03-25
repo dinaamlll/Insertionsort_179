636bb5152a3ac867db905146dda3116f4be07e81
dinaamlll <dinamaliaa17@gmail.com>
2023-03-24 16:38:01 -0800

    membuat inputan jumlah elemen array dan memanggil variabel

--- InsertionSort/InsertionSort.cpp
+++ InsertionSort/InsertionSort.cpp
@@ -5,3 +5,8 @@ int arr[20];								// Membuat Array dengan panjang data 20
 int n;									// Membuat Variable inputan n
 
 void input () {						// Procedure Input
+	while (true)
+	{
+		cout << "Masukkan Jumlah Data pada Array : ";// Membuat Inputan Jumlah Elemen Array
+		cin >> n;									// Memanggil Variable inputan n
+	}
