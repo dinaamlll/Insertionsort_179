33a4e3957c4635ce11e251bf4be95bfa4ae6570c
dinaamlll <dinamaliaa17@gmail.com>
2023-03-24 17:42:04 -0800

    membuat variabel

--- InsertionSort/InsertionSort.cpp
+++ InsertionSort/InsertionSort.cpp
@@ -23,6 +23,7 @@ void input() {						// Procedure Input
 	cout << "Masukkan Element Array" << endl;
 	cout << "======================" << endl;
 
+
 	for (int i = 0; i < n; i++)						// Menggunakan perulangan for untuk menyimpan data pada array
 	{
 		cout << "Data ke-" << ": ";					// Masukkan nilai data n kedalam array arr
@@ -35,7 +36,18 @@ void insertionsort() {						// Procedure Insertionsort
 	int temp;								// Membuat variable data temporer atau penyimpanan sementara
 	int j;									// Membuat variable j sebagai penanda
 
-	for (int i = 1; 1 < n; i++) {
+	for (int i = 1; 1 < n; i++) {			// 1. Looping dengan i dimulai dari 1 hingga n-1
 
-	}
-}
+		temp = arr[i];						// 2. Simpan nilai arr [i] ke variable sementara temp
+
+		j = i - 1;							// 3. Setting nilai j sama dengan i-1;
+
+		while (j >= 0 && arr[j] > temp)		// 4. Looping whhile dimana nilai j lebih besar sama dengan 0 dan	
+											// arr[j] lebih besar daripada temp
+		{
+			arr[j + 1] = arr[j];			// 4a. Simpan arr[j] ke dalam variable arr [ j + 1 ]
+			j--;							// 4b. Decrement nilai j by 1
+		}
+
+		arr[j + 1] = temp;					// 5. Simpan nilai temp ke dalam arr [j+1
+		}
