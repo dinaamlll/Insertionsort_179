25aadc314fcecfb89c6236eaa6c658ebf4089e4f
dinaamlll <dinamaliaa17@gmail.com>
2023-03-24 17:14:35 -0800

    prosedur insertionsort

--- InsertionSort/InsertionSort.cpp
+++ InsertionSort/InsertionSort.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int arr[20];								// Membuat Array dengan panjang data 20
 int n;									// Membuat Variable inputan n
 
-void input () {						// Procedure Input
+void input() {						// Procedure Input
 	while (true)
 	{
 		cout << "Masukkan Jumlah Data pada Array : ";// Membuat Inputan Jumlah Elemen Array
@@ -20,10 +20,13 @@ void input () {						// Procedure Input
 	}
 	cout << endl;									// Membuat jarak per baris program
 	cout << "======================" << endl;	// Membuat tampilan susunan data element array
-	cout << "Masukkan Element Array" << endl;		
+	cout << "Masukkan Element Array" << endl;
 	cout << "======================" << endl;
 
 	for (int i = 0; i < n; i++)						// Menggunakan perulangan for untuk menyimpan data pada array
 	{
 		cout << "Data ke-" << ": ";					// Masukkan nilai data n kedalam array arr
+		cin >> arr[i];
 	}
+}
+
